c64bdc305eb6bd097b3bc2faf804242ca66eb6da
Ekaterina <kacaderevcova@gmail.com>
2024-10-02 14:16:09 +0200

    added all tests

--- library/library_test.go
+++ library/library_test.go
@@ -73,3 +73,137 @@ func TestLibrary_AddBook(t *testing.T) {
 		}
 	})
 }
+
+func TestLibrary_RemoveBook(t *testing.T) {
+	t.Run("test1", func(t *testing.T) {
+		var library *Library = NewLibrary()
+		var inputBook book.Book = *book.NewBook("Harry Potter", "J.K. Rowling")
+
+		library.AddBook(inputBook, 10)
+		library.RemoveBook(inputBook)
+
+		if len(library.books) == 0 {
+			assert.True(t, true)
+		} else {
+			assert.True(t, false)
+		}
+	})
+
+	t.Run("test2", func(t *testing.T) {
+		var library *Library = NewLibrary()
+		var inputBook1 book.Book = *book.NewBook("Harry Potter", "J.K. Rowling")
+		var inputBook2 book.Book = *book.NewBook("Little Women", "L.M. Alcott")
+
+		library.AddBook(inputBook1, 10)
+		library.AddBook(inputBook2, 12)
+		library.RemoveBook(inputBook1)
+
+		if len(library.books) == 1 {
+			assert.True(t, true)
+		} else {
+			assert.True(t, false)
+		}
+	})
+}
+
+func TestLibrary_RemoveBookByTitle(t *testing.T) {
+	t.Run("test1", func(t *testing.T) {
+		var library *Library = NewLibrary()
+		var inputBook book.Book = *book.NewBook("Harry Potter", "J.K. Rowling")
+
+		library.AddBook(inputBook, 10)
+		library.RemoveBookByTitle("Harry Potter")
+
+		if len(library.books) == 0 {
+			assert.True(t, true)
+		} else {
+			assert.True(t, false)
+		}
+	})
+
+	t.Run("test2", func(t *testing.T) {
+		var library *Library = NewLibrary()
+		var inputBook1 book.Book = *book.NewBook("Harry Potter", "J.K. Rowling")
+		var inputBook2 book.Book = *book.NewBook("Little Women", "L.M. Alcott")
+
+		library.AddBook(inputBook1, 10)
+		library.AddBook(inputBook2, 12)
+		library.RemoveBookByTitle("Harry Potter")
+
+		if len(library.books) == 1 {
+			assert.True(t, true)
+		} else {
+			assert.True(t, false)
+		}
+	})
+
+	t.Run("test3", func(t *testing.T) {
+		var library *Library = NewLibrary()
+		var inputBook1 book.Book = *book.NewBook("Harry Potter", "J.K. Rowling")
+		var inputBook2 book.Book = *book.NewBook("Little Women", "L.M. Alcott")
+
+		library.AddBook(inputBook1, 10)
+		library.AddBook(inputBook2, 12)
+		library.RemoveBookByTitle("Harry")
+
+		if len(library.books) == 2 {
+			assert.True(t, true)
+		} else {
+			assert.True(t, false)
+		}
+	})
+}
+
+func TestLibrary_BorrowBook(t *testing.T) {
+	t.Run("test1", func(t *testing.T) {
+		var library *Library = NewLibrary()
+		var inputBook book.Book = *book.NewBook("Harry Potter", "J.K. Rowling")
+		var n uint
+		n = 10
+
+		library.AddBook(inputBook, n)
+		library.BorrowBook(inputBook)
+
+		if library.books[inputBook] == n-1 {
+			assert.True(t, true)
+		} else {
+			assert.True(t, false)
+		}
+	})
+
+	t.Run("test2", func(t *testing.T) {
+		var library *Library = NewLibrary()
+		var inputBook1 book.Book = *book.NewBook("Harry Potter", "J.K. Rowling")
+		var inputBook2 book.Book = *book.NewBook("Little Women", "L.M. Alcott")
+		var n uint
+		n = 10
+
+		library.AddBook(inputBook1, n)
+		library.AddBook(inputBook2, n)
+		library.BorrowBook(inputBook1)
+
+		if library.books[inputBook1] == n-1 {
+			assert.True(t, true)
+		} else {
+			assert.True(t, false)
+		}
+	})
+
+	t.Run("test3", func(t *testing.T) {
+		var library *Library = NewLibrary()
+		var inputBook1 book.Book = *book.NewBook("Harry Potter", "J.K. Rowling")
+		var inputBook2 book.Book = *book.NewBook("Little Women", "L.M. Alcott")
+		var n uint
+		n = 10
+
+		library.AddBook(inputBook1, n)
+		library.AddBook(inputBook2, n)
+		library.BorrowBook(*book.NewBook("Harry", "J.K. Rowling"))
+
+		if library.books[inputBook1] == n {
+			assert.True(t, true)
+		} else {
+			assert.True(t, false)
+		}
+	})
+}
